be8599c4fb585538f21617052d9ce36f39ed81b5
blaxsior <heejun2798@gmail.com>
2022-04-10 13:44:39 +0900

    docker-compose을 이용하여 데이터베이스 컨테이너와 서버 연결

--- servers/server/src/db/db.index.ts
+++ servers/server/src/db/db.index.ts
@@ -1,11 +1,22 @@
-import {DataSource} from 'typeorm';
+import { DataSource } from 'typeorm';
+import Key from '../key.js';
+import { User } from './entity/user.entity.js';
 
-const db = new DataSource({
+let db: DataSource;
+
+db = new DataSource({
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    database: 'test',
-    password: 'password',
-    username: 'root',
-    
-})
+    host:  Key.DB_HOST,
+    username: Key.DB_USER,
+    database: Key.DB_DATABASE,
+    password: Key.DB_PASSWORD,
+    timezone: '+09:00',
+    entities: [User],
+    synchronize: true
+});
+
+export const initDB = async () => {
+    await db.initialize();
+}
+
+export default db;
--- servers/server/src/key.ts
+++ servers/server/src/key.ts
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv';
 
-const result = dotenv.config()
+const result = dotenv.config();
 
 const Key = {
+    DB_HOST : process.env.DB_HOST,
+    DB_USER : process.env.DB_USER,
+    DB_PASSWORD : process.env.DB_PASSWORD,
+    DB_DATABASE : process.env.DB_DATABASE
 };
 
 export default Key;
