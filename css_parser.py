00ab104268218d3ad1c14194aee2aee984555f4d
blaxsior <heejun2798@gmail.com>
2022-04-15 19:20:27 +0900

    개인 시간표  정보 추출하는 scrap_table_info 모듈 작성

---
+++ css_parser.py
@@ -0,0 +1,12 @@
+def css_parser(css_style : str) -> dict[str,float]:
+    styles = css_style.split(';')
+    result_dict = {}    
+    for style in styles:
+        if len(style) > 0:
+            style_name, style_val = [ x.strip(' ').replace("px","") for x in style.split(':')]
+            style_val = float(style_val)
+
+            result_dict[style_name] = style_val
+
+    return result_dict
+    
---
+++ lecture.py
@@ -0,0 +1,18 @@
+class Lecture:
+    name: str
+    professor: str
+    loc: list[tuple[int, str]]
+
+    def __init__(self, name: str, professor: str):
+        self.name = name
+        self.professor = professor
+        self.loc = []
+
+    def add_loc(self, day: int, start: float, end: float):
+        self.loc.append((day, start, end))
+
+    def get_locs(self):
+        print(self.loc)
+
+    def get_lec_info(self):
+        print(f"강의 : {self.name}, 교수님: {self.professor}, 강의(요일,시작,끝) : {self.loc}")
