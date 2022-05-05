4e2dc77df3232a0e914d33ef462d8b2932dbfd22
Byabya <thditkfkd@gmail.com>
2022-05-06 02:07:54 +0900

    fill number

--- block_practice.py
+++ block_practice.py
@@ -1,5 +1,6 @@
+import numpy as np
 #튜플 연산불가
-# 리스트형
+# 리스트형, 근데 2배씩 하면 이렇게 계속 진행해도 되는가
 data_list = [[0, 4.0, 5.0], [2, 3.0, 4.0]]
 print("Data List", str(data_list))
 # code to split it into 3 lists (요일리스트, 시작교시리스트, 끝교시리스트)
@@ -11,13 +12,31 @@ print("Day", str(day_b))
 print("aclass",str(sts))
 print("bclass",str(end))
 
-height = day_b[1]-day_b[0]
- ##비교하기부터
+#정사각형 배열 만들기
+height = day_b[1]-day_b[0]+1
+if end[0]-sts[0] > end[1]-sts[1] :
+    width = end[0]-sts[0]
+else:
+    width = end[1]-sts[1]
+# 0벡터 만들기
+if height > width :
+  basic = np.zeros((height,height))
+  print(basic)
+else :
+    basic = np.zeros((width,width))
+    print(basic)
 
 
 
-# printing result
 
+#def square(height):
+ #   if height == 2 :
+  #      block=
+  #  elif height ==3:
 
+   # elif height==4:
 
+    #elif height==5:
+
+  #  else:
 
