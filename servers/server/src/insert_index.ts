2a855692b6f6343070e8af8380190333b20c204a
blaxsior <heejun2798@gmail.com>
2022-05-20 14:55:30 +0900

    데이터 삽입하기 위해 데이터 쪼갬

--- servers/server/src/insert_index.ts
+++ servers/server/src/insert_index.ts
@@ -1,11 +1,32 @@
-import db, { initDB } from "./db/db.index.js";
+import { exit } from "process";
+import { initDB } from "./db/db.index.js";
 import { ITetroInfo } from "./interface/tetro_pool.interface.js";
 import { IUnivInfo } from "./interface/university.interface.js";
 import { insert_json } from "./util/insert_json.js";
 
-const dir = ['data', 'lectures.json'];
-const univ : IUnivInfo = {name: '동국대학교'};
-const tetro : ITetroInfo = {name:'컴퓨터공학과 수업', 'description':'2021년 2학기 및 2022년 1학기 컴퓨터공학과 수업 모음입니다.'};
+let target = process.env.TETRO_TARGET;
+let t_id: number;
+
+if (target) {
+    t_id = parseInt(target);
+}
+else {
+    console.error('숫자가 아님');
+    exit();
+}
+const file = `lectures${t_id+1}.json`;
+
+const univ: IUnivInfo = { name: '동국대학교' };
+
+const dir = ['data', file];
+
+const tetro1: ITetroInfo = { name: '컴공01', 'description': '2022년 1학기 학기 정보(01)' };
+const tetro2: ITetroInfo = { name: '컴공02', 'description': '2022년 1학기 학기 정보(02)' };
+const tetro3: ITetroInfo = { name: '컴공03', 'description': '2021년 2학기 학기 정보(03)' };
+const tetro4: ITetroInfo = { name: '컴공04', 'description': '2021년 2학기 학기 정보(04)' };
+
+const tetros = [tetro1, tetro2, tetro3, tetro4];
 
 await initDB();
-await insert_json(univ, tetro, dir);
+
+await insert_json(univ, tetros[t_id], dir);
