25779a6b381e47ddcba7e5c18f253e8c131045f4
blaxsior <heejun2798@gmail.com>
2022-05-23 10:32:03 +0900

    테스트 코드 추가

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
-myl = [1,None, 2,None, 3,None]
+from util.lecture import Lecture
+from util.scrap_table_info import get_user_TT_info
 
-print(myl, len(myl))
+if __name__ == "__main__":
+    # try :
+    id = input("id 입력 :")
+    password = input("패스워드 입력 :")
+    # tables = get_user_TT_info(id, password)
+    # # 정보를 잘 스크래핑 했는지 검사
+    # for name in tables :
+    #     print(name)
+    #     for lec in tables[name]:
+    #         lec.get_lec_info()
+    #     print()
 
-for v in myl:
-    if v == None:
-        myl.remove(v)
+    all_lectures: dict[str, list[Lecture]]
+    all_lectures = get_user_TT_info(id, password)
 
-print(myl)
-
-print("월화수"[0:2])
-print(len("   ".strip()))
+    for lec in all_lectures:
+        print(lec)
+        for l in all_lectures[lec]:
+            print(l.loc)
